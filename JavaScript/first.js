f1d99f582a2ad308ee4445dbe8b2b9f5e8aa2499
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-07-29 22:05:43 +0530

    Add files via upload

---
+++ JavaScript/first.js
@@ -0,0 +1,22 @@
+// alert("hello world")
+console.log("hey")
+console.log("code is running")
+
+var a = prompt("Enter your number");
+
+console.log("Your number is " + a);
+console.log("Your number is ", a);
+
+var isTrue = prompt("Your device is a mac?");
+
+if(isTrue == "Yes") {
+    console.log("my device is a mac")
+}else{
+        console.log("my device is a windows")
+}
+
+var isTrue1 = confirm("are you exit this site")
+
+if(isTrue1){
+    console.log("ok, this website is leave")
+}
