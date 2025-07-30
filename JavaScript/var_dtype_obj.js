c22c9df34b96cb84e0d1fccca3cbf1d9285f86f3
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-07-30 08:39:19 +0530

    var, let, data type, const, obj

---
+++ JavaScript/var_dtype_obj.js
@@ -0,0 +1,41 @@
+console.log("variable and data type")
+
+// var a = 5;
+// var b = 6;
+// var c = "Chinmay"
+
+// console.log(a + b);
+// console.log(typeof a);
+
+// const a1 = 6;
+// a1 = 7;
+// console.log(a1)
+// not allowed
+
+
+// locally scope
+// let a = 4;
+
+// {
+//     let a = 6;
+//     console.log(a)
+// }
+// console.log(a)
+
+
+// locally scope
+var a = 4;
+
+{
+    var a = 6;
+    console.log(a)
+}
+console.log(a)
+
+
+let o = {
+    'Name': 'Chinmay',
+    'Age': 19
+}
+o.networth = "1Tillion";
+console.log(o);
