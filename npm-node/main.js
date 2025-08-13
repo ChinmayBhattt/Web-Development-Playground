a265925c43d01bb0c7c807dd0d0799bf7b0bd1ff
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-08-13 19:20:22 +0530

    Add files via upload

--- npm-node/main.js
+++ npm-node/main.js
@@ -1,27 +1,31 @@
-const { error } = require("console")
-const fs  = require("fs")
-console.log(fs)
+const express = require('express')
+const app = express()
+const port = 3000
 
-console.log("Starting")
-// fs.writeFileSync("chinmay.txt", "This is a fs module file in js")
-console.log("Ending")
+app.use(express.static('public')) // make a public folder, and all files in the public folder user easily to access
 
+// app.get, app.put, app.delete, app.post(path, headler)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-//call back hell
-fs.writeFile("chinmay1.txt","hello world", () =>{ // used this
-    console.log(fs)
-    fs.readFile("chinmay.txt", (error, data) =>{
-        console.log(error, data.toString)
-    })
+app.get('/service', (req, res) => {
+  res.send('hackathons, workshops, sessions')
+})
+app.get('/about', (req, res) => {
+  res.send('hackers unity is indias leading tech community')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('chinmay bhatt, phone no')
+})
+app.get('/blog/:slug', (req, res) => {
+  console.log(req.params) //will output: { slug: 'helloworld' }
+  console.log(req.query) // will output: { mode: 'dark', region: 'in' }
+  res.send(`Hello ${req.params.slug}`)
 })
 
 
-fs.appendFile("chinmay.txt", "chinmaybhatt", (e, d)=>{ // error = e, data = d
-    console.log(d)
-
-})
-
-fs.appendFile("hello.txt", "hello world" , (error, data)=>{
-    console.log(d)
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
