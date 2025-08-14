a77541245670972899658b39bff7517f38ea2879
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-08-14 09:10:24 +0530

    Add files via upload

--- npm-node/main.js
+++ npm-node/main.js
@@ -2,29 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public')) // make a public folder, and all files in the public folder user easily to access
+app.use(express.static("public"));
 
-// app.get, app.put, app.delete, app.post(path, headler)
 app.get('/', (req, res) => {
+  console.log("Hey its a get request")
   res.send('Hello World!')
 })
 
-app.get('/service', (req, res) => {
-  res.send('hackathons, workshops, sessions')
+app.post('/', (req, res) => {
+  console.log("Hey its a post request")
+  res.send('Hello World!')
 })
-app.get('/about', (req, res) => {
-  res.send('hackers unity is indias leading tech community')
-})
-
-app.get('/contact', (req, res) => {
-  res.send('chinmay bhatt, phone no')
-})
-app.get('/blog/:slug', (req, res) => {
-  console.log(req.params) //will output: { slug: 'helloworld' }
-  console.log(req.query) // will output: { mode: 'dark', region: 'in' }
-  res.send(`Hello ${req.params.slug}`)
-})
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
