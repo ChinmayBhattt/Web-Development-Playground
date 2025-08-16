aed3a6b5b939847ba5a1096cf19f5afcab1493a0
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-08-16 12:01:45 +0530

    Add files via upload

--- npm-node/main.js
+++ npm-node/main.js
@@ -1,19 +1,70 @@
 const express = require('express')
+const blog = require('./routes/blog')
 const app = express()
-const port = 3000
+const port = 3000;
 
-app.use(express.static("public"));
+app.use('/blog', blog)
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  console.log("Hey its a get request")
-  res.send('Hello World!')
+app.get('/', (req, res)=>{
+    console.log('its a get request')
+    res.send("Hello World2")
 })
 
-app.post('/', (req, res) => {
-  console.log("Hey its a post request")
-  res.send('Hello World!')
+app.post('/', (req, res)=>{
+    console.log('its a post request')
+    res.send("Hello world post")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.put('/', (req, res)=>{
+    console.log('its a put request')
+    res.send("Hello world put 2")
+})
+
+app.delete('/', (req, res)=>{
+    console.log('its a delete request')
+    res.send("Hello world delete")
+})
+
+app.get('/index', (req, res)=>{
+    console.log("index file")
+    res.sendFile('templates/index.html', {root: __dirname})
+})
+
+app.get('/api', (req, res)=>{
+    res.json({a: 1, b:2, c:3, d: 4})
+})
+
+// app.get('/', (req, res)=>{
+//     console.log('its a get request')
+//     res.send("Hello World")
+// }).post('/', (req, res)=>{
+//     console.log('its a post request')
+//     res.send("Hello world post")
+// }).put('/', (req, res)=>{
+//     console.log('its a put request')
+//     res.send("Hello world put")
+// })
+
+// app.delete('/', (req, res)=>{
+//     console.log('its a delete request')
+//     res.send("Hello world delete")
+// })
+
+// app.get('/', (req, res)=>{
+//     console.log('its a get request')
+//     res.send("Hello World")
+// }).post('/', (req, res)=>{
+//     console.log('its a post request')
+//     res.send("Hello world post")
+// }).put('/', (req, res)=>{
+//     console.log('its a put request')
+//     res.send("Hello world put")
+// }).delete('/', (req, res)=>{
+//     console.log('its a delete request')
+//     res.send("Hello world delete")
+// })
+
+app.listen(port, ()=> {
+    console.log(`Example app listening on port ${port}`)
 })
