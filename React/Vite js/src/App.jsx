851af5b22510cc862c9eb16c42de4fa5aa05e5d6
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-08-31 00:36:14 +0530

    Add files via upload

---
+++ React/Vite js/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/navbar"
+import Footer from "./components/Footer"
+import Card from "./components/card"
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    
+    <div className="cards">
+    <Card title="card 1" description="card 1 description"/>
+    <Card title="card 2" description="card 2 description"/>
+    <Card title="card 3" description="card 3 description"/>
+    <Card title="card 4" description="card 4 description"/>
+
+    </div>
+    <Footer/>
+ 
+    </>
+  )
+}
+
+export default App
