5ab65a41ad6a1e2855a3edeea01da4b44b429905
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-09-02 19:10:01 +0530

    Add files via upload

---
+++ React/ContextAPI/withcontextAPI-hook/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const button = () => {
+  return (
+    <div>
+      <button> <span> <component1/> </span> I am a button </button>
+    </div>
+  )
+}
+
+export default button
---
+++ React/ContextAPI/withcontextAPI-hook/src/components/component1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from '../App'
+import { useState } from 'react'
+
+const component1 = ({count}) => {
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+export default component1
