aea579cb706f0e7975c5d9a1cb3cf7d5f02aaf11
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-09-03 19:59:39 +0530

    Add files via upload

---
+++ Nextjs/first/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     <h1 className='text-xl'>Problem Solved by Nextjs</h1>
+     <ul>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Additional Features like router from next/navigation</li>
+        <li>optimized rendering</li>
+     </ul>
+    </div>
+  )
+}
+
+export default page
